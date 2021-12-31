e12bf7f7337c445973b965ec56fd0a64679f979d
Al Sweigart <asweigart@gmail.com>
2021-12-31 13:07:00 -0600

    Refactored out the code from platform() since it's only called once.

--- getkey/__init__.py
+++ getkey/__init__.py
@@ -2,22 +2,17 @@ from __future__ import absolute_import, print_function
 import sys
 from .platforms import GetKeyException, PlatformUnix, PlatformWindows, windows_or_unix
 
-def platform():
-    if sys.platform.startswith('linux') or sys.platform.startswith('darwin'):
-        return PlatformUnix()
-    elif sys.platform.startswith('win32'):
-        return PlatformWindows()
-    elif sys.platform.startswith('cygwin'):
-        return windows_or_unix()
-    else:
-        raise GetKeyException('Unknown platform {!r}'.format(sys.platform))
-
-
-__platform = platform()
+if sys.platform.startswith('linux') or sys.platform.startswith('darwin'):
+    __platform = PlatformUnix()
+elif sys.platform.startswith('win32'):
+    __platform = PlatformWindows()
+elif sys.platform.startswith('cygwin'):
+    __platform = windows_or_unix()
+else:
+    raise GetKeyException('Unknown platform {!r}'.format(sys.platform))
 
 getkey = __platform.getkey
 keys = __platform.keys
-key = keys  # alias
 bang = __platform.bang
 
 __version__ = '0.6.5'
